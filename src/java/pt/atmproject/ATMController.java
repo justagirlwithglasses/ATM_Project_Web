0a89e5e96f281654be2557abfad597907d4b44a3
justagirlwithglasses <justagirlwithglasses724@gmail.com>
2023-02-24 18:00:02 +0000

    new updates

--- src/java/pt/atmproject/ATMController.java
+++ src/java/pt/atmproject/ATMController.java
@@ -1,9 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package pt.atmproject;
 
+import atmproject.data.Database;
 import atmproject.model.Cliente;
 import atmproject.model.Conta;
 import atmproject.model.Cartao;
@@ -15,66 +12,101 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Admin
- */
-@WebServlet(name = "ATMController", urlPatterns = {"/ATMController"})
-public class ATMController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+@WebServlet(name = "ATMController", urlPatterns = {"/ATMController"})
+
+public class ATMController extends HttpServlet 
+{
+    
+    //@EJB(lookup = "java:global/ATM_Project!atmproject.model.Servicos");
+    //Servicos servicos;
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         
-        //Form Criar Cliente
+        Object resultado = null;
         
-        Cliente novocliente = new Cliente
-        (
-
-            request.getParameter("ID_cliente"),
-            request.getParameter("nome"),
-            request.getParameter("NIF"),
-            request.getParameter("email"),
-            request.getParameter("profissao"),
-            request.getParameter("senha")
-        
-        );
-     
-         
-         //Form Abrir Conta
-        
-         Conta novaconta = new Conta
-        (
-
-            Integer.parseInt(request.getParameter("num_conta")),
-            Double.parseDouble(request.getParameter("saldo_conta")),
-            Conta.Tipo_Conta.valueOf(request.getParameter("tipo_conta"))
-        
-        );
-        
+        String servico = request.getParameter("servico");
                 
-        //Form Criar Cartão
-        
-         Cartao novocartao = new Cartao
-        (
+        switch (servico)
+        {
 
-            request.getParameter("num_cartao"),
-            request.getParameter("data_val"),
-            Cartao.Tipo_Cartao.valueOf(request.getParameter("tipo_cartao")),
-            request.getParameter("nome_titular"), 
-            Integer.parseInt(request.getParameter("cod_seg"))
-        
-        );
+            //Form Criar Cliente
+            
+            case "criar_cliente":       
+            {
+               
+                 Cliente novocliente = new Cliente
+                 (
+
+                     request.getParameter("ID_cliente"),
+                     request.getParameter("nome"),
+                     request.getParameter("NIF"),
+                     request.getParameter("email"),
+                     request.getParameter("profissao"),
+                     request.getParameter("senha")
+
+                 );
+                 
+                 Database.criar_cliente(novocliente);
+                 
                 
+                 resultado = novocliente;
+                 
+                break;
+            
+            }
+          
+             //Form Abrir Conta
+            
+            
+            case "abrir_conta":           
+            {
+
+                Conta novaconta = new Conta
+               (
+                   Integer.parseInt(request.getParameter("num_conta")),
+                   Double.parseDouble(request.getParameter("saldo_conta")),
+                   Conta.Tipo_Conta.valueOf(request.getParameter("tipo_conta"))
+
+               );
+                
+                Database.abrir_conta(novaconta);
+                
+                
+                resultado = novaconta;
+                
+                 break;
+            }
+    
+            //Form Criar Cartão
+            
+            case "criar_cartao":
+            {
+                          
+                    Cartao novocartao = new Cartao
+                   (
+
+                       request.getParameter("num_cartao"),
+                       request.getParameter("data_val"),
+                       Cartao.Tipo_Cartao.valueOf(request.getParameter("tipo_cartao")),
+                       request.getParameter("nome_titular"), 
+                       Integer.parseInt(request.getParameter("cod_seg"))
+
+                   );
+                    
+                    Database.criar_cartao(novocartao);
+                    
+                    
+                   resultado = novocartao;
+                    
+                  break;
+            }
+            
+        }
+                
+   
                 
                 
         try ( PrintWriter out = response.getWriter()) {
@@ -86,9 +118,10 @@ public class ATMController extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             //out.println("<h1>Servlet ATMController at " + request.getContextPath() + "</h1>");
-            out.println("Novo Cliente: " + novocliente);
-            out.println("Nova Conta: " + novaconta); 
-            out.println("Novo Cartão: " + novocartao);
+            //out.println("Novo Cliente: " + novocliente);
+            //out.println("Nova Conta: " + novaconta); 
+            //out.println("Novo Cartão: " + novocartao);
+            out.println("<p> "+resultado+" </p>");
             out.println("</body>");
             out.println("</html>");
         }
